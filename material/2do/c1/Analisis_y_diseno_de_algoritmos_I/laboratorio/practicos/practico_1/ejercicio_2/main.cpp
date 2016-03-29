c57725267510504ee73c8774afeb29fb64fdd3f3
Abel M. Osorio <abel.m.osorio@gmail.com>
2016-03-29 18:14:25 -0300

    Se renombró el directorio Analisis_y_diseño_de_algoritmos_I por Analisis_y_diseno_de_algoritmos_I por problemas en la compilación usando CodeBlocks. Se agrega el ejercicio 2.

---
+++ material/2do/c1/Analisis_y_diseno_de_algoritmos_I/laboratorio/practicos/practico_1/ejercicio_2/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  int x, y;
+
+  cout << "Ingrese dos números separados por un espacio" << endl;
+  cin >> x >> y;
+  cout << x << " por " << y << " es igual a: " << x * y << endl ;
+
+  return 0;
+}
