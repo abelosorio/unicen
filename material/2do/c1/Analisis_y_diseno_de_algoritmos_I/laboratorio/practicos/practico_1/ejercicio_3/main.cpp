40c9ef77651c33c30426762e20ef4f844fd4660b
Abel M. Osorio <abel.m.osorio@gmail.com>
2016-03-30 15:16:22 -0300

    Se agregó el ejercicio 3.

---
+++ material/2do/c1/Analisis_y_diseno_de_algoritmos_I/laboratorio/practicos/practico_1/ejercicio_3/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string cadena1, cadena2;
+    cout << "Ingrese dos cadenas separadas por un espacio: " << endl;
+    cin >> cadena1 >> cadena2;
+    cout << "Sus cadenas concatenadas son: " << cadena1 << cadena2 << endl;
+    return 0;
+}
