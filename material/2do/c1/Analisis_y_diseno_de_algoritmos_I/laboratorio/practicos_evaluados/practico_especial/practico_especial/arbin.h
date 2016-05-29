a6cbeffcf4ec297a7478ef4ad68990c84d358a8e
Abel M. Osorio <abel.m.osorio@gmail.com>
2016-05-29 19:57:18 -0300

    Nuevos avances..

--- material/2do/c1/Analisis_y_diseno_de_algoritmos_I/laboratorio/practicos_evaluados/practico_especial/practico_especial/arbin.h
+++ material/2do/c1/Analisis_y_diseno_de_algoritmos_I/laboratorio/practicos_evaluados/practico_especial/practico_especial/arbin.h
@@ -1,1 +1,12 @@
+#include <iostream>
 
+using namespace std;
+
+template <typename Elem>
+class Arbin {
+    private:
+        struct Nodo {
+            Elem elemento;
+            Nodo * subIzquierdo;
+            Nodo * subDerecho;
+        };
